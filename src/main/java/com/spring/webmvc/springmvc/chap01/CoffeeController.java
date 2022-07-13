f3c10ce773e26b4fff4bdeb33021aa8ac94ea6ff
hwseo0705 <julieseo0705@gmail.com>
2022-07-13 11:04:32 +0900

    [07.13.22] Spring Coffee Commit

--- src/main/java/com/spring/webmvc/springmvc/chap01/CoffeeController.java
+++ src/main/java/com/spring/webmvc/springmvc/chap01/CoffeeController.java
@@ -1,15 +1,30 @@
 package com.spring.webmvc.springmvc.chap01;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Log4j2
 @RequestMapping("/coffee")
 public class CoffeeController {
 
-    @RequestMapping("/menu")
+    @RequestMapping("/form")
     public String form() {
+        log.info("/coffee/form GET Request");
         return "chap01/coffee-form";
+    }
+
+
+    @RequestMapping("/result")
+    public String result(String menu, int price, Model model) {
+        log.info("/coffee/result POST Request : [" + menu + ", " + price + "]");
+
+        model.addAttribute("menu", menu);
+        model.addAttribute("p", price);
+
+        return "chap01/coffee-result";
     }
 
 }
--- src/main/java/com/spring/webmvc/springmvc/chap01/LoginController.java
+++ src/main/java/com/spring/webmvc/springmvc/chap01/LoginController.java
@@ -2,7 +2,6 @@ package com.spring.webmvc.springmvc.chap01;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
     /*
