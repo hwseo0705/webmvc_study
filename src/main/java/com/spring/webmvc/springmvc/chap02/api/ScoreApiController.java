3cb2bf9983b75028a5b4e583754e9a0519a003cd
hwseo0705 <julieseo0705@gmail.com>
2022-07-29 13:46:41 +0900

    [07.29.22] yea idk

--- src/main/java/com/spring/webmvc/springmvc/chap02/api/ScoreApiController.java
+++ src/main/java/com/spring/webmvc/springmvc/chap02/api/ScoreApiController.java
@@ -5,7 +5,6 @@ import com.spring.webmvc.springmvc.chap02.repository.ScoreMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.List;
